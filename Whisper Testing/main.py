ec4dca2b916479991a9927562ef451c0e202b2f6
Hishamk <67921181+Hishamk2@users.noreply.github.com>
2024-07-16 12:04:59 -0500

    Add timings

--- Whisper Testing/main.py
+++ Whisper Testing/main.py
@@ -1,7 +1,15 @@
+import time
+
+start_time = time.time()
+print('Start program.')
+
+
 import os
 import whisper
 from shutil import which
 from prettify_txt import prettify_txt
+
+
 
 # print(whisper.available_models())
 
@@ -18,12 +26,20 @@ if not which("ffmpeg"):
         exit(1)
 
 # Load the Whisper model with GPU support
-model = whisper.load_model("large")
+# ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large']
+# TODO: .en means fine-tuned on English data
+model_size = "large"
+# base model sucks
+# small took 22.46 seconds
+# medium took 70.64 seconds
+# large took 161.56 seconds
+model = whisper.load_model(model_size)
 
 # Transcribe the audio file with language specified and word timestamps
 result = model.transcribe(r"C:\Users\hamza\OneDrive - University of Manitoba\Documents\HISHAM\Computer Science\Transcription Project\Whisper Testing\test.mp3", fp16=False, language="ur", word_timestamps=True)
+# result = model.transcribe(r"C:\Users\hamza\Downloads\test1.mp3", fp16=False, language="ur", word_timestamps=True)
 
-output_file_name = 'transcription-large.txt'
+output_file_name = f'transcription-{model_size}.txt'
 # Write the transcription to a text file with utf-8 encoding
 with open(output_file_name, "w", encoding="utf-8") as f:
     f.write(result["text"])
@@ -36,3 +52,9 @@ with open(output_file_name, "w", encoding="utf-8") as f:
 # Prettify the transcription text file
 prettify_txt(output_file_name)
 
+
+
+end_time = time.time()
+
+# Print the total execution time
+print(f"Execution time: {end_time - start_time:.2f} seconds")

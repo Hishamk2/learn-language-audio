b43ffd00e47fdf53cbfe64f6eb697a94837e3267
Hisham Kidwai <HishamSurgeon@gmail.com>
2024-08-12 13:16:50 -0500

    Change name to properly reflect stuff

--- electron/renderer.js
+++ electron/renderer.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('audio');
 
-    window.electron.receive('file-selected', (filePath) => {
+    window.electron.receive('file-opened', (filePath) => {
         audio.src = filePath;
         audio.play();
     });

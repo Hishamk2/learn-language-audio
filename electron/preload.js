7e16c05c20eda49b5d6cbdec7fcf72ebcdaade7e
Hisham Kidwai <HishamSurgeon@gmail.com>
2024-08-18 19:52:14 -0500

    Change srt to txt

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,10 @@
+// preload.js
+
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
     receive: (channel, func) => {
-        let validChannels = ['file-opened', 'srt-files-loaded']; // List of channels you want to allow
+        let validChannels = ['file-opened', 'txt-files-loaded']; // List of channels you want to allow
         if (validChannels.includes(channel)) {
             ipcRenderer.on(channel, (event, ...args) => func(...args));
         }
